9c10c606977875d384333a891b33e50c9a80d0e4
luisescareno18 <luisescareno1516@gmail.com>
2023-06-03 14:33:35 -0600

    Create Interfaz.py

---
+++ Interfaz.py
@@ -0,0 +1,33 @@
+from tkinter import *
+
+raiz= Tk()
+
+raiz.title("Desarrollo de problemas de Engranes")
+
+raiz.iconbitmap("logo upiiz.ico") #Imagen de fondo
+
+raiz.geometry("800x600") # Tamaño de ventana por defecto
+
+miFrame=Frame(raiz, width=800, height=600, bg="gray")
+
+miFrame.pack()
+MiImagen=PhotoImage(file="logo ipn (2).png")
+MiImagen2=PhotoImage(file="logo upiiz.png")
+
+Label(miFrame, text="UPIIZ", fg="#5e2129", font=("Serif", 30), bg="gray").place(x=350,y=20)
+
+Label(miFrame, text="Desarrollo de problemas de engranes", fg="#5e2129", font=("Serif", 20), bg="gray").place(x=150,y=80)
+
+Label(image=MiImagen, bg="gray").place(x=650,y=10)
+
+Label(image=MiImagen2, bg="gray").place(x=5,y=10)
+
+Label(miFrame, text="Resolver problema 1", font=("Serif", 14), bg="gray").place(x=50,y=150)
+
+Label(miFrame, text="Resolver problema 2", font=("Serif", 14), bg="gray").place(x=300,y=150)
+
+Label(miFrame, text="Resolver problema 3", font=("Serif", 14), bg="gray").place(x=600,y=150)
+
+Label(miFrame, text="UPIIZ", fg="#5e2129", font=("Serif", 30), bg="gray").place(x=350,y=20)
+
+raiz.mainloop()
